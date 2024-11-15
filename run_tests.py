397ac23fcb784aefcc68dedb8cbee587df62923f
martin <martin.colot@ulb.be>
2024-11-15 18:01:25 +0100

    testing LDA-KM-DA

--- run_tests.py
+++ run_tests.py
@@ -17,11 +17,13 @@ from sklearn.model_selection import cross_val_score, KFold
 import numpy as np
 import tensorflow as tf
 
-from dataLoader import load_ninaproDB4, load_ToroOssaba, load_EMG_EPN_612
-from timeDomainFeatures import computeTDF, normalizeTDF
-from UDA.MDD import fit_MDD
-from UDA.CDEM import CDEM
-from UDA.VADA_DIRTT import VADA_DIRTT
+from testingEnvironment.dataLoader import load_ninaproDB4, load_ToroOssaba, load_EMG_EPN_612
+from testingEnvironment.timeDomainFeatures import computeTDF, normalizeTDF
+from testingEnvironment.UDA.MDD import fit_MDD
+from testingEnvironment.UDA.CDEM import CDEM
+from testingEnvironment.UDA.VADA_DIRTT import VADA_DIRTT
+from ldaKmDa import LDA_KM_DA
+
 
 # ------------ PARAMETERS ------------------------------------------------------------------------------
 
@@ -267,10 +269,94 @@ if "VADA and DIRT-T" in testModels:
     scores["DIRT-T"] = scoreDIRTT
 
 if "LDA-KM-DA" in testModels:
-    pass
+    score = []
+    for test in range(len(X)):
+        train = np.arange(len(X))
+        train = np.delete(train, test)
+        if testFeatures == "TDF":
+            cl = LogisticRegression('l2', solver="liblinear")
+            cl.fit(np.concatenate([x for x in tdfX_n[train]]), np.concatenate([y for y in Y[train]]).astype(int))
+            pseudoLabels = cl.predict(tdfX_n[test])
+            cl = LDA_KM_DA(pseudoLabels, tdfX_n[test], eps=0.01, yTest=Y[test].astype(int), maxEpochs=100, plotResults=False)
+            score.append(np.mean(Y[test].astype(int) == cl.predict(tdfX_n[test])))
+        if testFeatures == "CMTS":
+            cl = LogisticRegression('l2', solver="liblinear")
+            cl.fit(np.concatenate([x for x in cmtsX[train]]), np.concatenate([y for y in Y[train]]).astype(int))
+            pseudoLabels = cl.predict(cmtsX[test])
+            cl = LDA_KM_DA(pseudoLabels, cmtsX[test], eps=0.01, yTest=Y[test].astype(int), maxEpochs=100, plotResults=False)
+            score.append(np.mean(Y[test].astype(int) == cl.predict(cmtsX[test])))
+        elif testFeatures == "Xdawn CMTS":
+            cov = XdawnCovariances(nfilter=3, estimator='oas').fit(np.concatenate(X[train]), np.concatenate(Y[train].astype(int)))
+            xTrain = [cov.transform(x) for x in X[train]]
+            xTest = cov.transform(X[test])
+            tsTrain = np.concatenate([TangentSpace('riemann').fit_transform(x) for x in xTrain])
+            tsTest = TangentSpace('riemann').fit_transform(xTest)
+            cl = LogisticRegression('l2', solver="liblinear").fit(tsTrain, np.concatenate(Y[train].astype(int)))
+            pseudoLabels = cl.predict(tsTest)
+            cl = LDA_KM_DA(pseudoLabels, cmtsX[test], eps=0.01, yTest=Y[test].astype(int), maxEpochs=100, plotResults=False)
+            score.append(np.mean(Y[test].astype(int) == cl.predict(cmtsX[test])))
+        print(test, score[-1])
+    print("average score LDA-KM-DA :", np.mean(score))
+    scores["LDA-KM-DA"] = score
 
 if "MDD -> LDA-KM-DA" in testModels:
-    pass
+    score = []
+    for test in range(len(X)):
+        train = np.arange(len(X))
+        train = np.delete(train, test)
+        if testFeatures == "TDF":
+            cl = fit_MDD(np.concatenate(tdfX_n[train]), np.concatenate(Y[train]).astype(int), tdfX_n[test], hidden=50, epochs=10)
+            pseudoLabels = cl.predict(tdfX_n[test])
+            cl = LDA_KM_DA(pseudoLabels, tdfX_n[test], eps=0.01, yTest=Y[test].astype(int), maxEpochs=100, plotResults=False)
+            score.append(np.mean(Y[test].astype(int) == cl.predict(tdfX_n[test])))
+        if testFeatures == "CMTS":
+            cl = fit_MDD(np.concatenate(cmtsX[train]), np.concatenate(Y[train]).astype(int), cmtsX[test], hidden=50, epochs=10)
+            pseudoLabels = cl.predict(cmtsX[test])
+            cl = LDA_KM_DA(pseudoLabels, cmtsX[test], eps=0.01, yTest=Y[test].astype(int), maxEpochs=100, plotResults=False)
+            score.append(np.mean(Y[test].astype(int) == cl.predict(cmtsX[test])))
+        elif testFeatures == "Xdawn CMTS":
+            cov = XdawnCovariances(nfilter=3, estimator='oas').fit(np.concatenate(X[train]), np.concatenate(Y[train].astype(int)))
+            xTrain = [cov.transform(x) for x in X[train]]
+            xTest = cov.transform(X[test])
+            tsTrain = np.concatenate([TangentSpace('riemann').fit_transform(x) for x in xTrain])
+            tsTest = TangentSpace('riemann').fit_transform(xTest)
+            cl = fit_MDD(tsTrain, np.concatenate(Y[train]).astype(int), tsTest, hidden=50, epochs=10)
+            pseudoLabels = cl.predict(tsTest)
+            cl = LDA_KM_DA(pseudoLabels, cmtsX[test], eps=0.01, yTest=Y[test].astype(int), maxEpochs=100, plotResults=False)
+            score.append(np.mean(Y[test].astype(int) == cl.predict(cmtsX[test])))
+        print(test, score[-1])
+    print("average score MDD -> LDA-KM-DA :", np.mean(score))
+    scores["MDD -> LDA-KM-DA"] = score
 
 if "Simplified CDEM -> LDA-KM-DA" in testModels:
-    pass
+    options = {
+        'beta': 1,
+        'lambda': 1,
+        'gamma': 0.5,
+        'eta': 0.0001,
+        'sigma': 0.1
+    }
+    score = []
+    for test in range(len(X)):
+        train = np.arange(len(X))
+        train = np.delete(train, test)
+        if testFeatures == "TDF":
+            pseudoLabels = CDEM(np.concatenate(tdfX_n[train]), np.concatenate(Y[train]).astype(int), tdfX_n[test], Y[test].astype(int), 10, 10, True, options)
+            cl = LDA_KM_DA(pseudoLabels, tdfX_n[test], eps=0.01, yTest=Y[test].astype(int), maxEpochs=100, plotResults=False)
+            score.append(np.mean(Y[test].astype(int) == cl.predict(tdfX_n[test])))
+        if testFeatures == "CMTS":
+            pseudoLabels = CDEM(np.concatenate(cmtsX[train]), np.concatenate(Y[train]).astype(int), cmtsX[test], Y[test].astype(int), 10, 10, True, options)
+            cl = LDA_KM_DA(pseudoLabels, cmtsX[test], eps=0.01, yTest=Y[test].astype(int), maxEpochs=100, plotResults=False)
+            score.append(np.mean(Y[test].astype(int) == cl.predict(cmtsX[test])))
+        elif testFeatures == "Xdawn CMTS":
+            cov = XdawnCovariances(nfilter=3, estimator='oas').fit(np.concatenate(X[train]), np.concatenate(Y[train].astype(int)))
+            xTrain = [cov.transform(x) for x in X[train]]
+            xTest = cov.transform(X[test])
+            tsTrain = np.concatenate([TangentSpace('riemann').fit_transform(x) for x in xTrain])
+            tsTest = TangentSpace('riemann').fit_transform(xTest)
+            pseudoLabels = CDEM(tsTrain, np.concatenate(Y[train]).astype(int), tsTest, Y[test].astype(int), 10, 10, True, options)
+            cl = LDA_KM_DA(pseudoLabels, cmtsX[test], eps=0.01, yTest=Y[test].astype(int), maxEpochs=100, plotResults=False)
+            score.append(np.mean(Y[test].astype(int) == cl.predict(cmtsX[test])))
+        print(test, score[-1])
+    print("average score simplified CDEM -> LDA-KM-DA :", np.mean(score))
+    scores["simplified CDEM -> LDA-KM-DA"] = score
